aaa46e6f44735cec935c48f66dbb1dcfc7b57855
heisqueenson9 <150591005+heisqueenson9@users.noreply.github.com>
2024-05-26 13:45:43 -0700

    make Enock Queenson bold

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is Enock Queenson</Text>
+      <Text style={styles.text}>My name is <Text style={styles.boldFont}>Enock Queenson</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,5 +19,8 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 24
+  },
+  boldFont: {
+    fontWeight: 'bold'
   }
 });
